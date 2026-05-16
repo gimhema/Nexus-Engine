7c071e329170f2644cd20a41e285291a2be8feae
kimmo <hemakim0215@naver.com>
2026-05-16 10:40:22 +0900

    .

--- NexusEngine/NexusEngine/Game/Logic/Item.h
+++ NexusEngine/NexusEngine/Game/Logic/Item.h
@@ -7,6 +7,26 @@
 #include <vector>
 #include <atomic>
 
+
+// class Singleton {
+// private:
+//     Singleton() {}
+//     Singleton(const Singleton& ref) {}
+//     Singleton& operator=(const Singleton& ref) {}
+//     ~Singleton() {}
+// public:
+//     static Singleton& getIncetance() {
+//         static Singleton s;
+//         return s;
+//     }
+// };
+
+// int main(void) {
+//     Singleton& s = Singleton::getIncetance();
+//     return 0;
+// }
+
+
 enum class ITEM_TYPE
 {
     DEFAULT,
@@ -39,19 +59,42 @@ class ItemBase
 };
 
 
+
 class ItemIDGenerator
 {
-    public:
-        ItemIDGenerator()
-        {
-            
-        }
-        ~ItemIDGenerator()
-        {
+public:
+    ItemIDGenerator(){}
+    ~ItemIDGenerator(){}
+    ItemIDGenerator(const ItemIDGenerator& ref) {}
+    ItemIDGenerator& operator=(const ItemIDGenerator& ref) {}
 
-        }
-    public:
+public:
+    static ItemIDGenerator& getIncetance() {
+        static ItemIDGenerator s;
+        return s;
+    }
+
+private:
+	static std::atomic<unsigned int> s_Sequence;
+
+public:
+	static unsigned long long Generate(unsigned short serverId)
+	{
+		unsigned long long uid = 0;
+
+		unsigned long long timestamp = static_cast<unsigned long long>(time(NULL));
+		unsigned long long seq = s_Sequence.fetch_add(1, std::memory_order_relaxed);
+
+		uid |= (static_cast<unsigned long long>(serverId) << 48);
+		uid |= ((timestamp & 0xFFFFFFFF) << 16);
+		uid |= (seq & 0xFFFF);
+
+		return uid;
+	}
+    
 };
+
+// std::atomic<unsigned int> ItemUIDGenerator::s_Sequence(0);
 
 class ItemCreator
 {
