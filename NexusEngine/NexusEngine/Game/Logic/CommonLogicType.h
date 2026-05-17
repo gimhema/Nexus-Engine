6c5be3b4cf723ea866c51741a8ffde01858b009f
kimmo <hemakim0215@naver.com>
2026-05-17 12:14:23 +0900

    .

--- NexusEngine/NexusEngine/Game/Logic/CommonLogicType.h
+++ NexusEngine/NexusEngine/Game/Logic/CommonLogicType.h
@@ -9,5 +9,16 @@ enum class SKIN_PARTS_TYPE
     _BODY_DOWN = 3,
     _HAND = 4,
     _SHOES = 5,
-    _END = 6
+    _END
+};
+
+enum class EQUIPMENT_POS_TYPE
+{
+    _DEFAULT = 0,
+    _WEAPON = 1,
+    _ARMOR_HEAD = 2,
+    _ARMOR_BODY = 3,
+    _ARMOR_HAND = 4,
+    _ARMOR_SHOES = 5,    
+    _END
 };
--- NexusEngine/NexusEngine/Game/Logic/Pawn/PlayerItemComponent.h
+++ NexusEngine/NexusEngine/Game/Logic/Pawn/PlayerItemComponent.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <unordered_map>
 #include <array>
 #include "CommonLogicType.h"
 
 #define MAX_CONSUMABLE_BAG_NUM 120
 #define MAX_SKIN_BAG_NUM 120
 #define MAX_EQUIPMENT_BAG_NUM 120
+
+#define MAX_QUICKSLOT_CONSUMBALE 10
 
 class Skin;
 class Equipment;
@@ -26,5 +27,7 @@ class ItemComponent
 
     // Use Slot
     public:
-
+        std::array<Skin*, static_cast<int>(SKIN_PARTS_TYPE::_END)> CurrentSkins;
+        std::array<Equipment*, static_cast<int>(EQUIPMENT_POS_TYPE::_END)> CurrentEquipments;
+        std::array<Consumbale*, MAX_QUICKSLOT_CONSUMBALE> ConsubaleQuickSlot;        
 };
