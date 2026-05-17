ee44fb3af601230381f5f0977fa2f3635a7c5e78
kimmo <hemakim0215@naver.com>
2026-05-17 12:21:13 +0900

    .

--- NexusEngine/NexusEngine/Game/Logic/Pawn/PlayerItemComponent.h
+++ NexusEngine/NexusEngine/Game/Logic/Pawn/PlayerItemComponent.h
@@ -25,6 +25,25 @@ class ItemComponent
         std::array<Equipment*, MAX_EQUIPMENT_BAG_NUM> EquipmentBag;
         std::array<Consumbale*, MAX_CONSUMABLE_BAG_NUM> ConsumableBag;
 
+    // Bag Action
+    public:
+        void AddSkinToBag(Skin* newSkin, int Pos);
+        void AddEquipmentToBag(Equipment* newEquipment, int Pos);
+        void AddConsumableToBag(Consumbale* newCons, int Pos);
+
+        void DropSkinFromBag(int Pos);
+        void DropEquipmentFromBag(int Pos);
+        void DropConsumableFromBag(int Pos);
+
+        void SwapSkin(int Pos1, int Pos2);
+        void SwapEquipment(int Pos1, int Pos2);
+        void SwapConsumable(int Pos1, int Pos2);
+
+        void SyncSkinBag();
+        void SyncEquipmentBag();
+        void SyncConsumableBag();
+ 
+
     // Use Slot
     public:
         std::array<Skin*, static_cast<int>(SKIN_PARTS_TYPE::_END)> CurrentSkins;
