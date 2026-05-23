b471f0d72ebce284a8c150150a9dd54cc99e5961
kimmo <hemakim0215@naver.com>
2026-05-23 10:48:35 +0900

    .

--- NexusEngine/NexusEngine/Game/Logic/Equipment/Equipment.h
+++ NexusEngine/NexusEngine/Game/Logic/Equipment/Equipment.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "Item.h"
+
+
+class Equipment : public ItemBase
+{
+    public:
+        Equipment() {}
+        ~Equipment() {}       
+  
+    protected:
+        void Create(ItemBasicInfo iInfo) override;
+        void Use() override;
+    
+    public:
+        void Attach(EQUIPMENT_POS_TYPE parts);
+        void Detach(EQUIPMENT_POS_TYPE parts);
+        
+};
