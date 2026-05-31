c44ab12476ea64d68581c4815965de0e512fb389
kimmo <hemakim0215@naver.com>
2026-05-31 11:16:08 +0900

    .

--- NexusEngine/NexusEngine/protocol_shared/Packets/Packet-Item.h
+++ NexusEngine/NexusEngine/protocol_shared/Packets/Packet-Item.h
@@ -7,25 +7,59 @@
 
 #include <cstdint>
 
-    // SMSG_STORE_ITEM,                   // S-C (TCP) 아이템 획득시
-    // CMSG_DROP_ITEM,                    // C-S (TCP) 아이템 드랍 요청
-    // SMSG_DROP_ITEM,                    // S-C (TCP) 아이템 드랍 요청 서버처리
-
-    // // Consumable Action
-    // CMSG_USE_CONSUMABLE,
-    // SMSG_USE_CONSUMABLE,
-    
-    // // Skin Action
-    // CMSG_USE_SKIN,
-    // SMSG_USE_SKIN,
-
-    // // Equipment Action
-    // CMSG_USE_EQUIPMENT,
-    // SMSG_USE_EQUIPMENT,
-
+// 아이템 정보를 담아서 보낼수있는 별도의 통신용 구조체가 필요함
 
 struct SMsg_StorItem
 {
 
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static SMsg_StorItem              Decode(NexusPacketParser& r);
+};
+struct CMsg_DropItem
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static CMsg_DropItem              Decode(NexusPacketParser& r);
+};
+
+struct SMsg_DropItem
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static SMsg_DropItem              Decode(NexusPacketParser& r);
+};
+
+struct CMsg_UseConsumable
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static CMsg_UseConsumable              Decode(NexusPacketParser& r);
+};
+
+struct SMsg_UseConsumable
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static SMsg_UseConsumable              Decode(NexusPacketParser& r);
+};
+
+struct CMsg_UseSkin
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static CMsg_UseSkin              Decode(NexusPacketParser& r);
+};
+
+struct SMsg_UseSkin
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static SMsg_UseSkin              Decode(NexusPacketParser& r);
+};
+
+struct CMsg_UseEquipment
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static CMsg_UseEquipment              Decode(NexusPacketParser& r);
+};
+
+struct SMsg_UseEquipment
+{
+    [[nodiscard]] NexusByteArray  Encode() const;
+    static SMsg_UseEquipment              Decode(NexusPacketParser& r);
 };
 
