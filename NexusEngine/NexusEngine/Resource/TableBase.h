dd3cb712a0df6c727bb848add38aa246d616b48f
kimmo <hemakim0215@naver.com>
2026-08-30 22:43:04 +0900

    .

--- NexusEngine/NexusEngine/Resource/TableBase.h
+++ NexusEngine/NexusEngine/Resource/TableBase.h
@@ -1,1 +1,30 @@
 #pragma once
+
+// Like CSV
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+typedef int TKey;
+typedef std::string TType;
+typedef std::string TValue;
+
+class TData
+{
+private:
+
+
+public:
+
+};
+
+class TableBase
+{
+public:
+	TableBase() = default;
+	virtual ~TableBase() = default;
+	virtual void LoadFromFile(const std::string& filePath) = 0;
+	virtual void SaveToFile(const std::string& filePath) const = 0;
+
+};
